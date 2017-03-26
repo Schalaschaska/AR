abb923b68e96b5855ab28a6a6664bfb465ab8b23
Schalaschaska <mihanhlystow@gmail.com>
2017-03-26 18:39:42 +0300

    Com 2

--- AR/CreateWin.xaml.cs
+++ AR/CreateWin.xaml.cs
@@ -12,31 +12,72 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.IO;
+using System.Text.RegularExpressions;
+using NetOffice.WordApi.Enums;
+using Microsoft.VisualStudio.TextTemplating.VSHost;
+using System.Globalization;
+
+
 
 namespace AR
 {
     /// <summary>
     /// Логика взаимодействия для CreateWin.xaml
     /// </summary>
+    /// 
+
     public partial class CreateWin : Window
     {
         public CreateWin()
         {
             InitializeComponent();
+
             
         }
         
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            FileStream file = new FileStream("history.txt", FileMode.Append, FileAccess.Write);
-            StreamWriter strem = new StreamWriter(file);
-            strem.WriteLine(textBox.Text);
-            strem.Close();
-            file.Close();
-            Editor editor = new Editor();
-            editor.Show();
-            this.Close();
             
+            Regex Reg_z = new Regex(@"^[a-zA-Z0-9]+$");
+            if (Reg_z.IsMatch(textBox.Text))
+            {
+                FileStream file = new FileStream("history.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter strem = new StreamWriter(file);
+                strem.WriteLine(textBox.Text);
+                strem.Close();
+                file.Close();
+                NetOffice.WordApi.Application word = new NetOffice.WordApi.Application();
+                word.DisplayAlerts = WdAlertLevel.wdAlertsNone;
+                NetOffice.WordApi.Document newdoc = word.Documents.Add();
+                word.Selection.TypeText("Test text");
+                word.Selection.HomeKey(WdUnits.wdLine, WdMovementType.wdExtend);
+                word.Selection.Font.Color = WdColor.wdColorAqua;
+                word.Selection.Font.Bold = 1;
+                word.Selection.Font.Size = 18;
+                
+
+
+                Editor editor = new Editor();
+                editor.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Ошибка ввода!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
         }
+        #region Helder
+        
+        private static string GetDefaultExtension(NetOffice.WordApi.Application application)
+        {
+            double version = Convert.ToDouble(application.Version, CultureInfo.InvariantCulture);
+            if (version >= 12.00)
+                return ".docx";
+            else
+                return ".doc";
+        }
+
+        #endregion
     }
 }
--- AR/Editor.xaml.cs
+++ AR/Editor.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
 namespace AR
 {
     /// <summary>
@@ -25,7 +26,18 @@ namespace AR
             
 
         }
-        
+        public double yi=0;//Уравнения профиля коронки
+        public double Vi=0;
+        public double di= 0.0011;
+        public double ai=0;
+        public double a=40;
+        public double q = 16000.0;
+        public double qi = 0;
+        public double Ri= 0.020;
+        public double R0= 0.020;
+        public double Ki=0;
+        public double f=0.30;
+        public int n=1;
         private void Start_click(object sender, RoutedEventArgs e)
         {
             
@@ -33,8 +45,15 @@ namespace AR
             start_page.Show();
             this.Close();
         }
-       
-       
 
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            yi = 2*a * (Ri - R0);
+            Vi = Math.Pow((1 + Math.Pow(yi, 2)), -(1 / 2));
+            qi = (di * Vi * q) / (36 * di * Math.Pow(Vi, 2));
+            a = 2 * 3.141593 * f * qi * Ri;
+            MessageBox.Show(Convert.ToString(qi));
+            MessageBox.Show(Convert.ToString(a));
+        }
     }
 }
