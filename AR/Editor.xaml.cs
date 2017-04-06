78bf70e8c22b0304ea2760cacea69083f0c09a1b
Schalaschaska <mihanhlystow@gmail.com>
2017-04-06 18:02:44 +0300

    Com 5

--- AR/Editor.xaml.cs
+++ AR/Editor.xaml.cs
@@ -20,6 +20,7 @@ using System.Threading;
 
 
 
+
 namespace AR
 {
     /// <summary>
@@ -33,18 +34,20 @@ namespace AR
             
 
         }
-        public double yi=0;//Уравнения профиля коронки
+        public double yi;//Уравнения профиля коронки
         public double Vi=0;
         public double di= 0.0011;
         public double ai=0;
-        public double a;
+        public double a=40.1;
         public double q;
         public double qi = 0;
-        public double Ri= 0.020;
-        public double R0;
+        public double Ri= 0.0200;
+        //double[] RI = new double[] { };
+        public double R0= 0.020;
         public double Ki=0;
         public double f;
         public int n;
+        double H = 0.0002;
         public double A;
         double[] Di = new double[] { 0.0240, 0.02574, 0.02750, 0.02925, 0.031, 0.03275, 0.0345, 0.03625, 0.038 };
         int[] KI = new int[] { 36, 36, 36, 36, 24, 32, 32, 32, 32 };
@@ -60,11 +63,11 @@ namespace AR
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            f = Convert.ToDouble(k_t.Text);
+            /*f = Convert.ToDouble(k_t.Text);
             n = Convert.ToInt32(k_s.Text);
             R0 = Convert.ToDouble(v_r.Text);
             a = Convert.ToDouble(p_p.Text);
-            q = Convert.ToDouble(o_n.Text);
+            q = Convert.ToDouble(o_n.Text);*/
 
             /*NetOffice.WordApi.Application word = new NetOffice.WordApi.Application();
             word.DisplayAlerts = WdAlertLevel.wdAlertsNone;
@@ -73,31 +76,79 @@ namespace AR
             
 
             word.Selection.HomeKey(WdUnits.wdLine, WdMovementType.wdExtend);
-            word.Selection.Font.Color = WdColor.wdColorAqua;
+            word.Selection.Font.Color = WdColor.wdColorAqua;//тистим цвет
             word.Selection.Font.Bold = 1;
             word.Selection.Font.Size = 18;
-            string fileExtension = GetDefaultExtension(word);
+            string fileExtension = GetDefaultExtension(word);//проверка версии
             object documentFile = string.Format("{0}\\Test{1}", Directory.GetCurrentDirectory(), fileExtension);
             newdoc.SaveAs(documentFile);
             word.Quit();
             word.Dispose();*/
             if (n_1.IsChecked == true)
             {
+                List<double> RI_list = new List<double> { };
+                List<double> YI_list = new List<double> { };
+                List<double> VI_list = new List<double> { };
+                List<double> QI_list = new List<double> { };
+                List<double> AI_list = new List<double> { };
+                List<double> Sum_list = new List<double> { };
+                RI_list.Add(Ri);
+                while (Ri < 0.038)
+                {
+
+                    Ri = Ri + H;
+                    RI_list.Add(Ri);
+                    //MessageBox.Show(Convert.ToString(Ri));
+                    
+
+                }
+                double[] RI = RI_list.ToArray<double>();
+                for (int i = 0; i <= RI.Length-1; i++)
+                {
+                    yi = 2 * a * (RI[i] - R0);
+                    YI_list.Add(yi);
+                    //MessageBox.Show(Convert.ToString(RI[i]));
+                    
+                }
+                double[] YI = YI_list.ToArray<double>();
+                for(int i=0;i<=YI.Length-1;i++)
+                {
+                    Vi= Math.Pow((1 + Math.Pow(YI[i], 2)), -(1 / 2));
+                    VI_list.Add(Vi);
+                }
+                double[] VI = VI_list.ToArray<double>();
+                for(int i=0;i<=VI.Length-1;i++)
+                {
+                    pr_sum = pr_sum + (KI[i] * di * Math.Pow(VI[i], 2));
+                   
+                }
+                for(int i=0;i<=VI.Length-1;i++)
+                {
+                    qi = (di * VI[i] * q) / pr_sum;
+                    QI_list.Add(qi);
+                }
+                double[] QI = QI_list.ToArray<double>();
+                for(int i=0;i<=QI.Length-1;i++)
+                {
+                    A= A = 2 * 3.141593 * f * QI[i] * RI[i];
+                    AI_list.Add(A);
+                }
+                double[] AI = AI_list.ToArray<double>();
+
+            }
+           if(n_2.IsChecked==true)
+            {
                 yi = 2 * a * (Ri - R0);
-                Vi = Math.Pow((1 + Math.Pow(yi, 2)), -(1 / 2)); 
-                for(int i=0;i<=n-1;i++)
+                Vi = Math.Pow((1 + Math.Pow(yi, 2)), -(1 / 2));
+                for (int i = 0; i <= n - 1; i++)
                 {
                     pr_sum = pr_sum + (KI[i] * di * Math.Pow(Vi, 2));
                 }
-                
+
                 qi = (di * Vi * q) / pr_sum;
                 A = 2 * 3.141593 * f * qi * Ri;
                 MessageBox.Show(Convert.ToString(qi));
                 MessageBox.Show(Convert.ToString(A));
-            }
-           if(n_2.IsChecked==true)
-            {
-
             }
         }
         #region Helder
